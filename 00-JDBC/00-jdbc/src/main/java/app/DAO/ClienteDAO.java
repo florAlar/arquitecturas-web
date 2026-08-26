9f2237f4c70793788dec69da9ec5690d843b670d
Pamela Loustaunau <pamelitaloustaunau@gmail.com>
2026-08-26 13:45:50 -0300

    Agrego entidad y DAO de Cliente

--- 00-JDBC/00-jdbc/src/main/java/app/DAO/ClienteDAO.java
+++ 00-JDBC/00-jdbc/src/main/java/app/DAO/ClienteDAO.java
@@ -1,4 +1,14 @@
 package app.DAO;
 
-public class ClienteDAO {
+import app.Entidades.Cliente;
+import  java.util.List;
+
+
+public interface ClienteDAO {
+    Cliente findByIdCliente(Long idCliente);
+    List <Cliente> findAllClientes();
+    void create(Cliente c);
+    void update(Cliente c);
+    void delete(Long idCliente);
+    void deleteAll();
 }
--- 00-JDBC/00-jdbc/src/main/java/app/Entidades/Cliente.java
+++ 00-JDBC/00-jdbc/src/main/java/app/Entidades/Cliente.java
@@ -1,4 +1,15 @@
 package app.Entidades;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class Cliente {
+    private Long idCliente;
+    private String nombre;
+    private String email;
 }
