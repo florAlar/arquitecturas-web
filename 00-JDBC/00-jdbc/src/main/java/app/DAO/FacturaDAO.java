de8b2a72ea2514b553e3cc497fbf7154e5008c33
Soledad <solcedro82@gmail.com>
2026-08-27 18:30:52 -0300

    agrego Interface y Entidad Factura

--- 00-JDBC/00-jdbc/src/main/java/app/DAO/FacturaDAO.java
+++ 00-JDBC/00-jdbc/src/main/java/app/DAO/FacturaDAO.java
@@ -1,4 +1,15 @@
 package app.DAO;
+import app.Entidades.Factura;
+import java.util.List;
 
-public class FacturaDAO {
+public interface FacturaDAO {
+    Factura getFacturaById(Long idFactura);
+    Factura getFacturaByClienteId(Long idCliente);
+    List<Factura> getAllFacturas();
+    void insertFactura(Factura factura);
+    void updateFactura(Factura factura);
+    void deleteFactura(Long idFactura);
+
+
+
 }
