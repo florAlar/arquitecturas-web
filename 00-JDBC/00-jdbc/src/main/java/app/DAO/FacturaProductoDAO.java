bba4ff7f3999d6d817b72149eff3337ea12038b8
John David Molina <jd_v@yahoo.com>
2026-09-02 10:24:55 -0300

    Entidad FacturaProducto y FacturaProductoDAO realizado, haciendo MYSQLFacturaProductoDAO

--- 00-JDBC/00-jdbc/src/main/java/app/DAO/FacturaProductoDAO.java
+++ 00-JDBC/00-jdbc/src/main/java/app/DAO/FacturaProductoDAO.java
@@ -3,5 +3,11 @@ package app.DAO;
 import java.sql.Connection;
 
 public interface FacturaProductoDAO {
-
+    FacturaProducto getById(int idFactura, int idProducto);
+    List<FacturaProducto> getByIdFactura(int idFactura);
+    List<FacturaProducto> getByIdProducto(int idProducto);
+    List<FacturaProducto> getAll();
+    void insert(FacturaProducto facturaProducto);
+    void update(FacturaProducto facturaProducto);
+    void delete(int idFactura, int idProducto);
 }
--- 00-JDBC/00-jdbc/src/main/java/app/Entidades/FacturaProducto.java
+++ 00-JDBC/00-jdbc/src/main/java/app/Entidades/FacturaProducto.java
@@ -1,4 +1,15 @@
 package app.Entidades;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class FacturaProducto {
+    private int idFactura;
+    private int idProducto;
+    private int cantidad;
 }
