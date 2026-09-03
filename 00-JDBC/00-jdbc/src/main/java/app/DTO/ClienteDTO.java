99d5ef1d45e889aedf31e083f05d08a775593310
Nicolás Azuaga <nicolas.azuaga@redb.ee>
2026-09-03 16:41:51 -0300

    feat(jdbc): crear DTOs para consultas integradoras

--- 00-JDBC/00-jdbc/src/main/java/app/DTO/ClienteDTO.java
+++ 00-JDBC/00-jdbc/src/main/java/app/DTO/ClienteDTO.java
@@ -1,4 +1,37 @@
 package app.DTO;
 
 public class ClienteDTO {
+    private String nombre;
+    private String email;
+    private float totalFacturado;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(String nombre, String email, float totalFacturado) {
+        this.nombre = nombre;
+        this.email = email;
+        this.totalFacturado = totalFacturado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public float getTotalFacturado() {
+        return totalFacturado;
+    }
+
+    @Override
+    public String toString() {
+        return "ClienteDTO{" +
+                "nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", totalFacturado=" + totalFacturado +
+                '}';
+    }
 }
--- 00-JDBC/00-jdbc/src/main/java/app/DTO/ProductoDTO.java
+++ 00-JDBC/00-jdbc/src/main/java/app/DTO/ProductoDTO.java
@@ -1,4 +1,37 @@
 package app.DTO;
 
 public class ProductoDTO {
+    private String nombre;
+    private float valor;
+    private float recaudacion;
+
+    public ProductoDTO() {
+    }
+
+    public ProductoDTO(String nombre, float valor, float recaudacion) {
+        this.nombre = nombre;
+        this.valor = valor;
+        this.recaudacion = recaudacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public float getRecaudacion() {
+        return recaudacion;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoDTO{" +
+                "nombre='" + nombre + '\'' +
+                ", valor=" + valor +
+                ", recaudacion=" + recaudacion +
+                '}';
+    }
 }
